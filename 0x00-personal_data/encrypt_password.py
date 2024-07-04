4281d6696aad7012c568e1ee892c6f12fa358295
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-04 21:36:09 +0000

    correct type for hashed_password paramter

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,6 +10,6 @@ def hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def is_valid(hashed_password: str, password: str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """ validate that the provided password matches the hashed password. """
     return bcrypt.checkpw(password.encode(), hashed_password)
