c54d4d732e04c7dc1bdc41c86035b3dc42f1897b
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-04 02:22:55 +0000

    add dock to the methods

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,10 +28,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Initiate new RedactingFormatter Object """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ change the format of record """
         record.msg = filter_datum(
             self.fields, self.REDACTION, record.msg, self.SEPARATOR
             )
