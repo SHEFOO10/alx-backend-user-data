60c5bb017e9363acefcea36e4bd554214ccbba60
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-04 19:12:05 +0000

    Update filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,7 +63,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     dbname = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    connection = mysql.connector.connect(
+    connection = mysql.connector.MySQLConnection(
         user=username,
         password=password,
         host=host,
