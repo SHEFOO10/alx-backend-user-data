3885f1280819371478387a4f74b42812ddfb1346
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-04 19:06:00 +0000

    set a default empty string if db not passed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,7 +62,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    dbname = os.getenv("PERSONAL_DATA_DB_NAME")
+    dbname = os.getenv("PERSONAL_DATA_DB_NAME", "")
     connection = mysql.connector.connect(
         user=username,
         password=password,
