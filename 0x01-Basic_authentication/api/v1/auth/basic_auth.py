9a41fff02687ea722bbca628edeb4995ffaeb735
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-08 20:35:15 +0000

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,18 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+         self,
+         base64_authorization_header: str
+         ) -> str:
+        """Decode base64 part from the Authorization Header """
+        if (
+            base64_authorization_header is None
+            or type(base64_authorization_header) is not str
+        ):
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except (Exception):
+            return None
