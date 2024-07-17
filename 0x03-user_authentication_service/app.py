da83ca26b3841e47ae83beb8a7bc83e37719cc83
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-17 18:46:23 +0000

    19. Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" A simple Flask app with authentication features """
+
+
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
@@ -76,6 +79,22 @@ def get_reset_password_token():
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": "{}".format(email),
                         "reset_token": "{}".format(reset_token)})
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """ Update user password by a valid token """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    if not email or not reset_token or not new_password:
+        abort(403)
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email,
+                        "message": "Password updated"})
     except ValueError:
         abort(403)
 
