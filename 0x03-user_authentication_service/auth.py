463add1aade915634c1f267be657235d30894bd6
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-17 16:36:16 +0000

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+""" Module for Authentication """
+
+
+import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
 from db import DB
 from user import User
 
@@ -22,6 +28,28 @@ class Auth:
         hashed_password = _hash_password(password)
         user = self._db.add_user(email, hashed_password)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Checks if email and password match a registered user
+
+        Args:
+            email (str): the email of the user
+            password (str): the password of the user
+        Returns:
+            bool: True if if email and password match a registered user
+            otherwise False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+            password_bytes = password.encode('utf-8')
+            hashed_password = user.hashed_password
+
+            if bcrypt.checkpw(password_bytes, hashed_password):
+                return True
+        except NoResultFound:
+            return False
 
 
 def _hash_password(password: str) -> bytes:
